96e1cd5453da3332b897a418336e9c72970eaef8
Tau Merand <tau.taylor.merand@gmail.com>
2021-08-31 17:04:34 +0200

    fixed val part of loo
    There will be an error though in save checkpoint

--- src/classification/chexpert.py
+++ src/classification/chexpert.py
@@ -27,12 +27,12 @@ def train_model(model,
                 criterion, 
                 optimizer, 
                 device, 
-                **save_args):
+                **save_cfg):
 
-  save_freq=save_args.pop("save_freq", len(train_loader)//2)
-  start_time=save_args.pop("start_time", time.time())
-  time_out=save_args.pop("time_out", 9*60*60)
-  threshold=save_args.pop("time_threshold", 0.8)
+  save_freq=save_cfg.pop("save_freq", len(train_loader)//2)
+  start_time=save_cfg.pop("start_time", time.time())
+  time_out=save_cfg.pop("time_out", 9*60*60)
+  threshold=save_cfg.pop("time_threshold", 0.8)
 
   model.train()
   batch_losses = []
@@ -52,7 +52,7 @@ def train_model(model,
       batch_losses.append(loss.item())
       if i % save_freq == 0:
         if time.time() - start_time > threshold*time_out:
-          save_checkpoint(model, train_loss=np.mean(batch_losses), **save_args)      
+          save_checkpoint(model, train_loss=np.mean(batch_losses), **save_cfg)      
       loop.set_postfix(loss=np.mean(batch_losses))
   else: #TODO TPU XLA code
     print("TPU")
@@ -60,11 +60,13 @@ def train_model(model,
   return np.mean(batch_losses)
 
 
-def eval_model(model, val_loader, criterion, device):
+def eval_model(model, 
+                val_loader,
+                criterion, 
+                device):
   model.eval()
   val_loss = 0.0
   if device.type == 'cuda' or device.type == 'cpu':
-    model.to(device)
     for images, labels in tqdm(val_loader, desc="Val Inner"):
         images = images.to(device)
         labels = labels.to(device)
@@ -80,7 +82,7 @@ def eval_model(model, val_loader, criterion, device):
 
 
 def vgg16_classifier(loader_cfg: Optional[Dict[str, str]] = None,
-                      saving_cfg: Optional[Dict[str, str]] = None,
+                      save_cfg: Optional[Dict[str, str]] = None,
                       weights: Optional[str] = "imagenet",
                       epochs: Optional[int] = 1,
                       device = torch.device("cuda" if torch.cuda.is_available() else "cpu"),
@@ -107,17 +109,12 @@ def vgg16_classifier(loader_cfg: Optional[Dict[str, str]] = None,
                           criterion, 
                           optimizer, 
                           device,
-                          **saving_cfg)
+                          **save_cfg)      
+    val_loss=eval_model(vgg16_classifier, val_loader, criterion, device)
 
-    # if time.time() - start_time > 0.9*time_out:
-      
-
-  #   curr_loss=eval_model(vgg16_classifier, val, criterion, device)
-
-  #   if curr_loss<min_val_loss:
-  #     filename=ckpt_dir+"/vgg_epoch_{}_train_{:.3f}_min_val_{:.3f}.pt".format(i,
-  #                                                                 t_loss,
-  #                                                                 curr_loss)
+    if val_loss<min_val_loss:
+      save_checkpoint(vgg16, epoch=i, train_loss=curr_loss, val_loss=val_loss, **save_cfg) 
+      min_val_loss=val_loss
   #     # print("Saving Model with validation loss: {:.3f}".format(curr_loss))
   #     torch.save(vgg16_classifier.state_dict, filename)
   #     min_val_loss=curr_loss
